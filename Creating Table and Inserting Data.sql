1e4cb48164abe7a736fba891c10806d05ba88399
Bhoja <135154482+BhojaByte@users.noreply.github.com>
2023-12-21 22:50:49 +0500

    Add files via upload

---
+++ Creating Table and Inserting Data.sql
@@ -0,0 +1,32 @@
+--CREATE TABLE EmployeeDemographics (
+--EmployyeeID int,
+--FirstName varchar(50),
+--LastName varchar(50),
+--Age int,
+--Gender varchar(50) )
+
+
+
+--CREATE TABLE EmployyeSalary (
+--EmployyeeID int,
+--JobTitle varchar(50),
+--Salary int )
+
+--INSERT INTO EmployyeeDemographics Values
+--(1005,'Khadija','Jawed',21,'Male'),
+--(1002,'Umaima','Saleem',17,'Female'),
+--(1003,'Fatimah','Mohammad',21,'Male'),
+--(1004,'Usama','Sheikh',21,'Male'),
+--(1006,'Farooq','Usman',21,'Male')
+
+--INSERT INTO EmployyeeSalary Values
+--(1001,'Salesmen',45000)
+
+--(1002, 'Receptionist', 36000),
+--(1003, 'Salesman', 63000),
+--(1004, 'Accountant', 47000),
+--(1005, 'HR', 50000),
+--(1006, 'Regional Manager', 65000),
+--(1007, 'Supplier Relations', 41000),
+--(1008, 'Salesman', 48000),
+--(1009, 'Accountant', 42000)
